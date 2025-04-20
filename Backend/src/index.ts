eb2fc33b861a4fb81aedb24db5b2918bd395f78d
Tanujeet <singhtanujeet@gmail.com>
2025-04-20 07:19:00 +0530

    Initial commit

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,14 +1,45 @@
 import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-// Create the main Hono app
-const app = new Hono();
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+  };
+}>();
 
-app.post("/api/v1/signup", (c) => {
-  return c.text("signup route");
+app.post("/api/v1/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  const body = await c.req.json();
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.passwords,
+      },
+    });
+    return c.text("jwt here");
+  } catch (e) {
+    return c.status(403);
+  }
 });
 
-app.post("/api/v1/signin", (c) => {
-  return c.text("signin route");
+app.post("/api/v1/signin", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  const body = await c.req.json();
+  const user = await prisma.user.findUnique({
+    where: {
+      email: body.email,
+    },
+  });
+  if (!user) {
+    c.status(403);
+    return c.json({ error: "user not found" });
+  }
 });
 
 app.get("/api/v1/blog/:id", (c) => {
