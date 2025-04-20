17fb49b9e93c10363c0002000e117c874d82aece
Tanujeet <singhtanujeet@gmail.com>
2025-04-20 10:19:26 +0530

    your message

--- Backend/src/routes/user.ts
+++ Backend/src/routes/user.ts
@@ -13,13 +13,13 @@ export const userRouter = new Hono<{
 
 userRouter.post("/signup", async (c) => {
   const body = await c.req.json();
-  //   const { success } = signupInput.safeParse(body);
-  //   if (!success) {
-  //     c.status(411);
-  //     return c.json({
-  //       message: "Inputs not correct",
-  //     });
-  //   }
+  // const { success } = signupInput.safeParse(body);
+  // if (!success) {
+  //   c.status(411);
+  //   return c.json({
+  //     message: "Inputs not correct",
+  //   });
+  // }
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
