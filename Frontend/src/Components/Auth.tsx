6f77d58afe95957aedd689db00deddc19d44e3e6
Tanujeet <singhtanujeet@gmail.com>
2025-04-21 07:28:15 +0530

    frontend created

--- Frontend/src/Components/Auth.tsx
+++ Frontend/src/Components/Auth.tsx
@@ -1,26 +1,30 @@
 import { Link } from "react-router-dom";
-import { ChangeEvent } from "react";
 import { useState } from "react";
 import { SignupInput } from "commonharsh";
+import LabelledInput from "./LabelledInput";
 
 const Auth = ({ type }: { type: "signup" | "signin" }) => {
-  const [postInput, setPostInput] = useState<SignupInput>({
+  const [postInputs, setPostInputs] = useState<SignupInput>({
     username: "",
     password: "",
     name: "",
   });
+
+  async function sendRequest() {}
   return (
     <div className="h-screen flex justify-center flex-col">
-      <div className=" flex justify-center">
+      <div className="flex justify-center">
         <div>
-          <div>
-            <div className=" text-4xl font-bold ">
-              {type === "signin" ? "Create a Account" : "Login your Account"}
+          <div className="px-10">
+            <div className="text-3xl font-extrabold">
+              {type === "signup"
+                ? "Create an Account"
+                : "Login your credentials"}
             </div>
-            <div className="text-slate-400">
+            <div className="text-slate-500">
               {type === "signin"
-                ? "Dont have a account ?"
-                : "Already have a account"}
+                ? "Don't have an account?"
+                : "Already have an account?"}
               <Link
                 className="pl-2 underline"
                 to={type === "signin" ? "/signup" : "/signin"}
@@ -29,39 +33,42 @@ const Auth = ({ type }: { type: "signup" | "signin" }) => {
               </Link>
             </div>
           </div>
-          <div>
+          <div className="pt-8">
+            {type === "signup" ? (
+              <LabelledInput
+                label="Name"
+                placeholder="Harkirat Singh..."
+                onChange={(e) => {
+                  setPostInputs({
+                    ...postInputs,
+                    name: e.target.value,
+                  });
+                }}
+              />
+            ) : null}
             <LabelledInput
               label="Username"
-              placeholder="Enter your username"
+              placeholder="harkirat@gmail.com"
               onChange={(e) => {
-                setPostInput((c) => ({
-                  ...c,
-                  name: e.target.value,
-                }));
-              }}
-            />
-            <LabelledInput
-              label="Email"
-              placeholder="me@example.com"
-              onChange={(e) => {
-                setPostInput((c) => ({
-                  ...c,
+                setPostInputs({
+                  ...postInputs,
                   username: e.target.value,
-                }));
+                });
               }}
             />
             <LabelledInput
               label="Password"
-              placeholder=""
               type={"password"}
+              placeholder="123456"
               onChange={(e) => {
-                setPostInput((c) => ({
-                  ...c,
+                setPostInputs({
+                  ...postInputs,
                   password: e.target.value,
-                }));
+                });
               }}
             />
             <button
+              onClick={sendRequest}
               type="button"
               className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
             >
@@ -73,35 +80,5 @@ const Auth = ({ type }: { type: "signup" | "signin" }) => {
     </div>
   );
 };
-
-interface LabelledInputType {
-  label: string;
-  placeholder: string;
-  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
-  type?: string;
-}
-
-function LabelledInput({
-  label,
-  placeholder,
-  onChange,
-  type,
-}: LabelledInputType) {
-  return (
-    <div>
-      <label className="block mb-2 text-sm text-black font-semibold pt-4">
-        {label}
-      </label>
-      <input
-        onChange={onChange}
-        type={type || "text"}
-        id="first_name"
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-        placeholder={placeholder}
-        required
-      />
-    </div>
-  );
-}
 
 export default Auth;
